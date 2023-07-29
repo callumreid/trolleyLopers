b1117652fe8177a3e6cee6a44df10c5e51d5b0cb
Callum Reid <callum.reid@volleythat.com>
2023-07-29 11:25:52 -0400

    Remove boiler plate stuff replace w dummy pic

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,4 @@
-<script>
-	import Counter from '$lib/Counter.svelte';
-</script>
+
 
 <svelte:head>
 	<title>Home</title>
@@ -8,50 +6,23 @@
 </svelte:head>
 
 <section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset="svelte-welcome.webp" type="image/webp" />
-				<img src="svelte-welcome.png" alt="Welcome" />
-			</picture>
-		</span>
 
-		to your new<br />SvelteKit app
-	</h1>
+		<img src="https://custom-skills-public.s3.amazonaws.com/trollilopolis/images/trolleyIntro.png" alt="scene of a trolley and a cow" /> 
 
 	<h2>
-		try editing <strong>src/routes/index.svelte</strong>
+		This Trolley Ain't For Riding
 	</h2>
 
-	<Counter />
+
 </section>
 
 <style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 1;
-	}
 
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
+	img {
 		position: absolute;
 		width: 100%;
 		height: 100%;
 		top: 0;
-		display: block;
+		display: flex;
 	}
 </style>
